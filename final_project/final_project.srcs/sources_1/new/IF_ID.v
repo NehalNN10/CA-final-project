ea882bfeae5e117d7795fbd8db01ea35072ad201
neeyazie <shawaizniazi917@gmail.com>
2024-04-20 15:45:08 +0500

    adding pipeline files for aina to wrk with

---
+++ final_project/final_project.srcs/sources_1/new/IF_ID.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/20/2024 03:36:16 PM
+// Design Name: 
+// Module Name: IF_ID
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module IF_ID(
+
+    );
+endmodule
---
+++ final_project/final_project.srcs/sources_1/new/MEM_WB.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/20/2024 03:36:16 PM
+// Design Name: 
+// Module Name: MEM_WB
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MEM_WB(
+
+    );
+endmodule
