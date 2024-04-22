42a47635ac09239fae33084a76e2edfb489cf350
Nehal Naeem Haji <nh07884@st.habib.edu.pk>
2024-04-22 16:55:37 +0500

    some further work done regarding pipelined sorting

--- final_project/final_project.sim/sim_1/behav/xsim/xsim.dir/tbHazard_behav/webtalk/xsim_webtalk.tcl
+++ final_project/final_project.sim/sim_1/behav/xsim/xsim.dir/tbHazard_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/Dell/Documents/uni_docs/cs/computer architecture/final_project/final_project/final_project.sim/sim_1/behav/xsim/xsim.dir/tbHazard_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Mon Apr 22 15:35:18 2024" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Mon Apr 22 16:51:52 2024" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.1 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2902540" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "4d1a6b53-e70d-4a28-8535-1312cef82c8f" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "2d87f27d44564ed9b8e24b103fb36c6a" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "21" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "28" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "11th Gen Intel(R) Core(TM) i7-1165G7 @ 2.80GHz" -context "user_environment"
@@ -36,7 +36,7 @@ webtalk_add_data -client xsim -key Total_Nets -value "0" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Total_Processes -value "114" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Total_Instances -value "25" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Xilinx_HDL_Libraries_Used -value "secureip unimacro_ver unisims_ver " -context "xsim\\usage"
-webtalk_add_data -client xsim -key Compiler_Time -value "1.75_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Compiler_Memory -value "68784_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1116774262 -regid "" -xml C:/Users/Dell/Documents/uni_docs/cs/computer architecture/final_project/final_project/final_project.sim/sim_1/behav/xsim/xsim.dir/tbHazard_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Dell/Documents/uni_docs/cs/computer architecture/final_project/final_project/final_project.sim/sim_1/behav/xsim/xsim.dir/tbHazard_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Dell/Documents/uni_docs/cs/computer architecture/final_project/final_project/final_project.sim/sim_1/behav/xsim/xsim.dir/tbHazard_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Compiler_Time -value "1.19_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Compiler_Memory -value "69036_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 4065295659 -regid "" -xml C:/Users/Dell/Documents/uni_docs/cs/computer architecture/final_project/final_project/final_project.sim/sim_1/behav/xsim/xsim.dir/tbHazard_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Dell/Documents/uni_docs/cs/computer architecture/final_project/final_project/final_project.sim/sim_1/behav/xsim/xsim.dir/tbHazard_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Dell/Documents/uni_docs/cs/computer architecture/final_project/final_project/final_project.sim/sim_1/behav/xsim/xsim.dir/tbHazard_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
